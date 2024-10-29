8409c8f57782230f785a2324a7f98de58d0f93e1
Nilesh-567 <137327289+Nilesh-567@users.noreply.github.com>
2024-10-29 13:47:22 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 app = Flask(__name__)
 
 # Configure MongoDB connection
-client = MongoClient("mongodb+srv://root:root@mydatabase.sgxomt2.mongodb.net/?retryWrites=true&w=majority&appName=mydatabase")
+client = MongoClient("mongodb+srv://mongodb:Nilesh123@mydatabase.sgxomt2.mongodb.net/?retryWrites=true&w=majority&appName=mydatabase")
 db = client["Vercel"]
 collection = db["flask"]
 
